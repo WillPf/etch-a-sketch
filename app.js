105b3805872ddeb140ef9143c15eabe805d1b350
WillJCabe <will.j.cabe@gmail.com>
2025-07-12 20:55:46 -1000

    Add color changes and new grid generation with 100 squares max

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const containerWidth = 900;
 let numOfSquares = 50;
-let divSize = containerWidth / numOfSquares;
-
 
 const container = document.querySelector(".container");
-for (let i = 0; i < numOfSquares; i++) {
-    for (let j = 0; j < numOfSquares; j++) {
-        let div = document.createElement("div");
-        div.style.cssText = `width: ${divSize}px; height: ${divSize}px`;
-        container.appendChild(div);
+const button = document.querySelector(".btn");
+const colorInput = document.querySelector(".colorInput");
+
+let color = colorInput.value;
+
+colorInput.addEventListener("change", () => color = colorInput.value)
+
+container.addEventListener("mouseover", (e) => {
+    e.target.style.cssText += `background-color: ${color}`;
+});
+
+button.addEventListener("click", () => {
+    numOfSquares = prompt("How many squares do you want?");
+    if (numOfSquares > 100) numOfSquares = 100;
+    generateGrid(numOfSquares);
+});
+
+function generateGrid(n) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let divSize = containerWidth / n;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            let div = document.createElement("div");
+            div.style.cssText = `width: ${divSize}px; height: ${divSize}px`;
+            container.appendChild(div);
+        }
     }
 }
-container.addEventListener("mouseover", (e) => {
-    e.target.style.cssText += "background-color: #8888ff";
-})
+generateGrid(numOfSquares);
